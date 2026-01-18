291f1a4acdb64a0236092757543c4f43ebbb0d9a
Sulaymon <xojaevsulaymon@gmail.com>
2026-01-18 16:52:53 +0500

    Icon bug solved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Icon from '../assets/Icon.png'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center h-20 bg-neutral-900 text-white mb-20'>
             <div className='flex justify-center items-center ml-5'>
-                <img src="./src/assets/Icon.png" alt="" className='h-40'/>
+                <img src={Icon} alt="" className='h-40'/>
             </div>
             <ul className='flex justify-center items-center gap-10 ml-10 text-lg font-bold mr-10'> 
                 <li className='hover:text-blue-500 hover:scale-150 duration-500'>
