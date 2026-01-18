3fe6d280f52abd78ef2cb60cd30b6fd5a452d5dd
Sulaymon <xojaevsulaymon@gmail.com>
2026-01-18 16:48:34 +0500

    Navbar changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center h-20 bg-neutral-900 text-white mb-20'>
-            <div className='pl-5'>
+            <div className='flex justify-center items-center ml-5'>
                 <img src="./src/assets/Icon.png" alt="" className='h-40'/>
             </div>
-            <ul className='flex justify-center items-center gap-10 ml-10 text-lg font-bold pr-10'> 
+            <ul className='flex justify-center items-center gap-10 ml-10 text-lg font-bold mr-10'> 
                 <li className='hover:text-blue-500 hover:scale-150 duration-500'>
                     <Link to="/">Home</Link>
                 </li>
